7b37d2e9e8ab7f5fc34c0e539982bb6fed9528f9
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-05 21:51:53 +0700

    Create logger.dart

---
+++ flutter_boilerplate/lib/config/logger.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:logger/logger.dart';
+
+class AppLogger {
+  final _logger = Logger(
+    filter: AppLogsFilter(),
+    printer: PrefixPrinter(
+      PrettyPrinter(
+        methodCount: 0,
+        errorMethodCount: 8,
+        lineLength: 200,
+        colors: false,
+        printEmojis: true,
+        printTime: false,
+      ),
+    ),
+    output: AppOutput(),
+  );
+
+  void trace(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.t(message, error: error, stackTrace: stackTrace);
+  }
+
+  void debug(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.d(message, error: error, stackTrace: stackTrace);
+  }
+
+  void info(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.i(message, error: error, stackTrace: stackTrace);
+  }
+
+  void warn(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.w(message, error: error, stackTrace: stackTrace);
+  }
+
+  void error(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.e(message, error: error, stackTrace: stackTrace);
+  }
+
+  void fatal(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.f(message, error: error, stackTrace: stackTrace);
+  }
+}
+
+class AppOutput extends LogOutput {
+  @override
+  void output(OutputEvent event) {
+    for (var line in event.lines) {
+      debugPrint(line);
+    }
+  }
+}
+
+class AppLogsFilter extends LogFilter {
+  @override
+  bool shouldLog(LogEvent event) {
+    if (kReleaseMode) {
+      return event.level.index >= Level.warning.index;
+    } else {
+      return true;
+    }
+  }
+}
