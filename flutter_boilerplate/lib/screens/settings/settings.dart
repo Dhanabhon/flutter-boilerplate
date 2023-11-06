71422e5c5c5423f1d5d17caa67282327c058e954
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-06 17:22:16 +0700

    Update settings.dart

--- flutter_boilerplate/lib/screens/settings/settings.dart
+++ flutter_boilerplate/lib/screens/settings/settings.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate/routes/routes.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:flutter_boilerplate/config/locator.dart';
+import 'package:flutter_boilerplate/config/logger.dart';
+import 'package:go_router/go_router.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final l10n = AppLocalizations.of(context)!;
+    final l10n = AppLocalizations.of(context);
+    final log = locator.get<AppLogger>();
 
     return Scaffold(
       body: Center(
           child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          DropdownButton<Locale>(
+            items: AppLocalizations.supportedLocales
+                .map<DropdownMenuItem<Locale>>((Locale value) {
+              return DropdownMenuItem<Locale>(
+                value: value,
+                child: Text(value.languageCode),
+              );
+            }).toList(),
+            onChanged: (locale) {},
+          ),
+          const SizedBox(
+            height: 16,
+          ),
           ElevatedButton(
-            onPressed: () {},
-            child: Text(''),
-          )
+            onPressed: () {
+              log.debug("The go back to home button has been pressed!");
+
+              context.go(AppRouter.root);
+            },
+            child: Text(l10n!.goBackHome),
+          ),
         ],
       )),
     );
