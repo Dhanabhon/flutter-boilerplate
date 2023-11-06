53c28e41fa9dad730e4bdb64f607d3771a70f938
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-06 17:57:45 +0700

    Update settings.dart

--- flutter_boilerplate/lib/screens/settings/settings.dart
+++ flutter_boilerplate/lib/screens/settings/settings.dart
@@ -16,12 +16,16 @@ class SettingsPage extends StatelessWidget {
     final log = locator.get<AppLogger>();
 
     return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.settings),
+      ),
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           DropdownButton<Locale>(
+            value: l10n.lo,
             items: AppLocalizations.supportedLocales
                 .map<DropdownMenuItem<Locale>>((Locale value) {
               return DropdownMenuItem<Locale>(
@@ -29,7 +33,9 @@ class SettingsPage extends StatelessWidget {
                 child: Text(value.languageCode),
               );
             }).toList(),
-            onChanged: (locale) {},
+            onChanged: (locale) {
+
+            },
           ),
           const SizedBox(
             height: 16,
