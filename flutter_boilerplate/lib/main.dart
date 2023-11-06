13ca03f72381f059352c7ef099567d0f1ef2a7cb
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-06 17:57:39 +0700

    Update main.dart

--- flutter_boilerplate/lib/main.dart
+++ flutter_boilerplate/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:flutter_boilerplate/bloc/language/language_bloc.dart';
 import 'package:flutter_boilerplate/config/locator.dart';
-import 'package:flutter_boilerplate/screens/home/home.dart';
 import 'package:flutter_boilerplate/routes/routes.dart';
 
 void main() {
