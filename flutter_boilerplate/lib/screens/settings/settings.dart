6680400929fc619b80a3cd7c361db6f6100fcd20
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-06 10:03:20 +0700

    Update settings.dart

--- flutter_boilerplate/lib/screens/settings/settings.dart
+++ flutter_boilerplate/lib/screens/settings/settings.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+
     return Scaffold(
       body: Center(
           child: Column(
-        children: [],
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(''),
+          )
+        ],
       )),
     );
   }
