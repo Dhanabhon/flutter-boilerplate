99075ddda5f55e06e5f688862ad8293e48adfcf9
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-06 17:22:22 +0700

    Update routes.dart

--- flutter_boilerplate/lib/routes/routes.dart
+++ flutter_boilerplate/lib/routes/routes.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate/screens/settings/settings.dart';
 
 import 'package:go_router/go_router.dart';
 
 import 'package:flutter_boilerplate/screens/page_not_found/page_not_found.dart';
 import 'package:flutter_boilerplate/screens/home/home.dart';
 
-final appRoutes = GoRouter(
-  navigatorKey: GlobalKey<NavigatorState>(),
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      name: 'home',
-      builder: (context, state) => const HomePage(),
-    ),
-  ],
-  errorBuilder: (context, state) => NotFoundPage(error: state.error!.message),
-);
+class AppRouter {
+  static const root = '/';
+  static const settings = '/settings';
+
+  static final _router = GoRouter(
+    navigatorKey: GlobalKey<NavigatorState>(),
+    initialLocation: root,
+    routes: [
+      GoRoute(
+        path: root,
+        name: 'home',
+        builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: settings,
+        name: 'settings',
+        builder: (context, state) => const SettingsPage(),
+      ),
+    ],
+    errorBuilder: (context, state) => NotFoundPage(error: state.error!.message),
+  );
+
+  static GoRouter get router => _router;
+}
