aa00725bb19108894863c38e3a7882e559fb4a28
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-07 10:16:19 +0700

    Update routes.dart

--- flutter_boilerplate/lib/routes/routes.dart
+++ flutter_boilerplate/lib/routes/routes.dart
@@ -1,32 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_boilerplate/screens/settings/settings.dart';
 
 import 'package:go_router/go_router.dart';
 
 import 'package:flutter_boilerplate/screens/page_not_found/page_not_found.dart';
 import 'package:flutter_boilerplate/screens/home/home.dart';
+import 'package:flutter_boilerplate/screens/onboarding/onboarding.dart';
+import 'package:flutter_boilerplate/screens/settings/settings.dart';
 
 class AppRouter {
-  static const root = '/';
-  static const settings = '/settings';
-
   static final _router = GoRouter(
     navigatorKey: GlobalKey<NavigatorState>(),
-    initialLocation: root,
+    initialLocation: Routes.onboardingPath,
     routes: [
       GoRoute(
-        path: root,
-        name: 'home',
+        name: Routes.root,
+        path: Routes.rootPath,
         builder: (context, state) => const HomePage(),
       ),
       GoRoute(
-        path: settings,
-        name: 'settings',
+        name: Routes.settings,
+        path: Routes.settingsPath,
         builder: (context, state) => const SettingsPage(),
       ),
+      GoRoute(
+        name: Routes.onboarding,
+        path: Routes.onboardingPath,
+        builder: (context, state) => const OnboardingPage(),
+      ),
     ],
+    redirect: (context, state) => null, // TODO: Add redirect based on something state.
     errorBuilder: (context, state) => NotFoundPage(error: state.error!.message),
   );
 
   static GoRouter get router => _router;
+}
+
+class Routes {
+  static const root = 'home';
+  static const rootPath = '/';
+  static const settings = 'settings';
+  static const settingsPath = '/settings';
+  static const onboarding = 'onboarding';
+  static const onboardingPath = '/onboarding';
 }
