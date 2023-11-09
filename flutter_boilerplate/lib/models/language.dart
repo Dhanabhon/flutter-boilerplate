d5af72e212c9fc4a04278cdd069c8d4c0aab8409
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-09 16:33:54 +0700

    Update language.dart

--- flutter_boilerplate/lib/models/language.dart
+++ flutter_boilerplate/lib/models/language.dart
@@ -3,20 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate/gen/assets.gen.dart';
 
 class LanguageModel {
-//   english(
-//     Locale('en', 'US'),
-//     // Assets.images.flags.unitedStatesOfAmerica,
-//     'English',
-//   ),
-//   thai(
-//     Locale('th', 'TH'),
-//     // Assets.images.flags.thailand,
-//     'Thai',
-//   );
-
+  
   final Locale locale;
   final AssetGenImage image;
   final String name;
 
-  const LanguageModel(this.locale, this.image, this.name);
+  const LanguageModel(
+      {required this.locale, required this.image, required this.name});
+
+  static LanguageModel english = LanguageModel(
+      locale: const Locale('en', 'US'),
+      image: Assets.images.flags.unitedStatesOfAmerica,
+      name: 'English');
+
+  static LanguageModel thai = LanguageModel(
+      locale: const Locale('th', 'TH'),
+      image: Assets.images.flags.thailand,
+      name: 'ภาษาไทย');
 }
