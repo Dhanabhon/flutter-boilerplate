56b943cfa81383ecc0527b2383e6b060071fea9a
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-13 05:44:19 +0700

    Update theme_bloc.dart

--- flutter_boilerplate/lib/bloc/theme/theme_bloc.dart
+++ flutter_boilerplate/lib/bloc/theme/theme_bloc.dart
@@ -3,7 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter_boilerplate/bloc/theme/theme_event.dart';
+import 'package:flutter_boilerplate/config/theme.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeData> {
-  const ThemeBloc();
+  ThemeBloc() : super(CustomTheme.lightTheme) {
+    on<InitializedTheme>((event, emit) async {});
+
+    on<SwitchedTheme>((event, emit) async {});
+  }
+
+  Future<bool> _isDarkMode() async {
+    // TODO:
+    return false;
+  }
+
+  Future<void> _onSetTheme(bool isDarkMode) async {
+    // TODO:
+    if (isDarkMode) {
+
+    } else {
+
+    }
+  }
 }
