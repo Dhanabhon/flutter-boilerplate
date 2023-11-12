96553479276ef9689bc0b37136433b12babe6cbb
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-13 05:44:27 +0700

    Update language_bloc.dart

--- flutter_boilerplate/lib/bloc/language/language_bloc.dart
+++ flutter_boilerplate/lib/bloc/language/language_bloc.dart
@@ -1,49 +1,38 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:flutter_boilerplate/config/locator.dart';
 import 'package:flutter_boilerplate/services/local_storage.dart';
+import 'package:flutter_boilerplate/utils/constants.dart';
 import 'package:flutter_boilerplate/bloc/language/language_event.dart';
 import 'package:flutter_boilerplate/bloc/language/language_state.dart';
 
 class LanguageBloc extends Bloc<LanguageEvent, LanguageState> {
   LanguageBloc() : super(const LanguageState(Language.english)) {
     on<ChangedLanguage>(_onChangedLanguage);
-    on<GetLanguage>(_onGetLanguage);
   }
 
   Future<void> _onChangedLanguage(
       ChangedLanguage event, Emitter<LanguageState> emit) async {
-    // final prefs = await SharedPreferences.getInstance();
-    // final savedLanguage = prefs.getString('language');
-
-    // final prefs = await LocalStorageService.getInstance();
-    final prefs = locator.get<LocalStorageService>();
-
+    final savedLanguage = await getLanguagePreference();
 
     if (savedLanguage != null) {
       final language =
           savedLanguage == 'english' ? Language.english : Language.thai;
 
       saveLanguagePreference(language);
-      emit(state.copyWith(event.language));
+      emit(state);
     }
   }
 
-  Future<void> _onGetLanguage(
-      GetLanguage event, Emitter<LanguageState> emit) async {
-    final prefs = await SharedPreferences.getInstance();
-    final language = prefs.getString('language');
-
-    emit(state.copyWith(
-      language != null ? 
-      Language.values
-      .where((item) => item.value.lan)));
+  Future<String?> getLanguagePreference() async {
+    final prefs = locator.get<LocalStorageService>();
+    return await prefs.readDataFromStorage(key: LocalStorageKey.language);
   }
 
   Future<void> saveLanguagePreference(Language language) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(
-        'language', language == Language.english ? 'english' : 'thai');
+    final prefs = locator.get<LocalStorageService>();
+    await prefs.saveDataToStorage(
+        key: LocalStorageKey.language,
+        value: language == Language.english ? 'english' : 'thai');
   }
 }
