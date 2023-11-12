ee4739c840f2c078179b568b86542ede2112a52b
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-12 21:54:19 +0700

    Create theme_bloc.dart

---
+++ flutter_boilerplate/lib/bloc/theme/theme_bloc.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:flutter_boilerplate/bloc/theme/theme_event.dart';
+
+class ThemeBloc extends Bloc<ThemeEvent, ThemeData> {
+  const ThemeBloc();
+}
