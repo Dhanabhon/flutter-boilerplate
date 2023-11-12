1d88feebc01c9597e3a0ad0925ef6dd43345fa24
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-12 19:22:58 +0700

    Update onboarding.dart

--- flutter_boilerplate/lib/screens/onboarding/onboarding.dart
+++ flutter_boilerplate/lib/screens/onboarding/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_boilerplate/bloc/language/language_bloc.dart';
 import 'package:flutter_boilerplate/bloc/language/language_state.dart';
