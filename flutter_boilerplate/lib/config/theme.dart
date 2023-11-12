f573c5847ca96694242a480c0585910cc6960b61
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-12 21:54:13 +0700

    Create theme.dart

---
+++ flutter_boilerplate/lib/config/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  static final lightTheme = ThemeData(
+    primaryColorLight: const Color(0xff486284),
+    scaffoldBackgroundColor: const Color(0xffF5F5F6),
+    buttonTheme: ButtonThemeData(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+      buttonColor: const Color(0xff486284),
+    ),
+  );
+
+  static final darkTheme = ThemeData(
+    primaryColorDark: const Color(0xff151f3c),
+    scaffoldBackgroundColor: const Color(0xffF5F5F6),
+    buttonTheme: ButtonThemeData(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+      buttonColor: const Color(0xff486284),
+    ),
+  );
+}
