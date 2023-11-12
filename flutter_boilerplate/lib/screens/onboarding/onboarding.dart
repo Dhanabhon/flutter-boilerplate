3a4f59c1eefaa79fc6df27f892a5b13eb1bd57d2
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-13 06:42:12 +0700

    Update onboarding.dart

--- flutter_boilerplate/lib/screens/onboarding/onboarding.dart
+++ flutter_boilerplate/lib/screens/onboarding/onboarding.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter_boilerplate/bloc/language/language_bloc.dart';
 import 'package:flutter_boilerplate/bloc/language/language_state.dart';
@@ -21,6 +20,7 @@ class OnboardingPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         actions: [
           Padding(
             padding: const EdgeInsets.symmetric(
@@ -29,9 +29,10 @@ class OnboardingPage extends StatelessWidget {
             ),
             child: OutlinedButton(
               onPressed: () {
-                // TODO:
                 log.debug(
-                    '[OnboardingPage]: Change language button has been tapped!');
+                    '[OnboardingPage]: Change language button has been pressed!');
+
+                showLanguageBottomSheet(context);
               },
               style: OutlinedButton.styleFrom(
                 padding: const EdgeInsets.all(8.0),
@@ -83,19 +84,91 @@ class OnboardingPage extends StatelessWidget {
                   const SizedBox(
                     height: 32.0,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      log.debug(
-                          '[OnboardingPage]: Get Started Button has been tapped!');
-                    },
-                    child: Text(l10n.getStartedText),
-                  )
                 ],
               ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                log.debug(
+                    '[OnboardingPage]: Get Started Button has been pressed!');
+              },
+              child: Text(l10n.getStartedText),
             )
           ],
         ),
       ),
+    );
+  }
+
+  void showLanguageBottomSheet(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+    final log = locator.get<AppLogger>();
+
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12.0), topRight: Radius.circular(12.0)),
+      ),
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                l10n.chooseLanguage,
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              const SizedBox(height: 16.0),
+              BlocBuilder<LanguageBloc, LanguageState>(
+                builder: (context, state) {
+                  return ListView.separated(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(Language.values[index].name == "english"
+                            ? "English"
+                            : "ภาษาไทย"),
+                        tileColor: Language.values[index] == state.language
+                            ? Colors.grey[400]?.withOpacity(0.05)
+                            : null,
+                        onTap: () {
+                          log.debug(
+                              '[OnboardingPage][showModalBottomSheet]: Item has been tapped!');
+                          // TODO:
+                        },
+                        leading: ClipOval(
+                          child: Language.values[index].name == "english"
+                              ? Assets.images.flags.unitedStatesOfAmerica
+                                  .image(height: 32.0, width: 32.0)
+                              : Assets.images.flags.thailand
+                                  .image(height: 32.0, width: 32.0),
+                        ),
+                        trailing: Language.values[index] == state.language
+                            ? const Icon(Icons.check_circle_outline_rounded,
+                                color: Colors.redAccent)
+                            : null,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side: Language.values[index] == state.language
+                              ? const BorderSide(
+                                  color: Colors.redAccent, width: 1.5)
+                              : BorderSide(color: Colors.grey[300]!),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(height: 16.0);
+                    },
+                    itemCount: Language.values.length,
+                  );
+                },
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
