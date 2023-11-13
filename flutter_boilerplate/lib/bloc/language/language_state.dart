bf4e124cd8c97167ff2b89ce54d97044b0253a54
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-14 00:24:53 +0700

    Update language_state.dart

--- flutter_boilerplate/lib/bloc/language/language_state.dart
+++ flutter_boilerplate/lib/bloc/language/language_state.dart
@@ -3,11 +3,11 @@ import 'package:equatable/equatable.dart';
 enum Language { english, thai }
 
 class LanguageState extends Equatable {
-  final Language language;
-  const LanguageState(this.language);
+  final Language selectedLanguage;
+  const LanguageState(this.selectedLanguage);
 
   @override
-  List<Object?> get props => [language];
+  List<Object?> get props => [selectedLanguage];
 
   Future<LanguageState> copyWith(Language language) async {
     return LanguageState(language);
