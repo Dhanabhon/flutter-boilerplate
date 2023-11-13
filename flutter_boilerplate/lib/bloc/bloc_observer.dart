947b72053b2f99d973980a8721a5da7f74029fce
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-13 21:56:15 +0700

    Update bloc_observer.dart

--- flutter_boilerplate/lib/bloc/bloc_observer.dart
+++ flutter_boilerplate/lib/bloc/bloc_observer.dart
@@ -9,24 +9,24 @@ class AppBlocServer extends BlocObserver {
   @override
   void onCreate(BlocBase bloc) {
     super.onCreate(bloc);
-    log.debug('[AppBlocServer][onCreate]: ${bloc.runtimeType}');
+    log.debug('[bloc_observer.dart][onCreate]: ${bloc.runtimeType}');
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    log.debug('[AppBlocServer][onChange]: ${bloc.runtimeType}, $change');
+    log.debug('[bloc_observer.dart][onChange]: ${bloc.runtimeType}, $change');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
     super.onError(bloc, error, stackTrace);
-    log.debug('[AppBlocServer][onError]: ${bloc.runtimeType}, $error');
+    log.debug('[bloc_observer.dart][onError]: ${bloc.runtimeType}, $error');
   }
 
   @override
   void onClose(BlocBase bloc) {
     super.onClose(bloc);
-    log.debug('[AppBlocServer][onClose]: ${bloc.runtimeType}');
+    log.debug('[bloc_observer.dart][onClose]: ${bloc.runtimeType}');
   }
 }
