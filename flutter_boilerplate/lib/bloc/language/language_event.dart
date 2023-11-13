a67d2341bb36299d8ad0d25e702d386838e5cc47
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-13 21:56:08 +0700

    Update language_event.dart

--- flutter_boilerplate/lib/bloc/language/language_event.dart
+++ flutter_boilerplate/lib/bloc/language/language_event.dart
@@ -11,7 +11,7 @@ abstract class LanguageEvent extends Equatable {
 
 class ChangedLanguage extends LanguageEvent {
   final Language language;
-  const ChangedLanguage(this.language);
+  const ChangedLanguage({required this.language});
 
   @override
   List<Object?> get props => [language];
