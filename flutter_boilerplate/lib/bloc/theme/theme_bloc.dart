3f714f2c94a6289a527aaa55a876d6ee7d5a1b10
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-13 20:18:28 +0700

    Update theme_bloc.dart

--- flutter_boilerplate/lib/bloc/theme/theme_bloc.dart
+++ flutter_boilerplate/lib/bloc/theme/theme_bloc.dart
@@ -3,26 +3,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter_boilerplate/bloc/theme/theme_event.dart';
+import 'package:flutter_boilerplate/config/locator.dart';
+import 'package:flutter_boilerplate/config/logger.dart';
 import 'package:flutter_boilerplate/config/theme.dart';
+import 'package:flutter_boilerplate/services/local_storage.dart';
+import 'package:flutter_boilerplate/utils/constants.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeData> {
-  ThemeBloc() : super(CustomTheme.lightTheme) {
-    on<InitializedTheme>((event, emit) async {});
+  final log = locator.get<AppLogger>();
 
-    on<SwitchedTheme>((event, emit) async {});
+  ThemeBloc() : super(CustomTheme.lightTheme) {
+    on<InitializedTheme>((event, emit) async {
+      if (await _isDarkMode()) {
+        emit(CustomTheme.darkTheme);
+      } else {
+        emit(CustomTheme.lightTheme);
+      }
+    });
+
+    on<SwitchedTheme>((event, emit) async {
+      final isDarkTheme = state == CustomTheme.darkTheme;
+      emit(isDarkTheme ? CustomTheme.lightTheme : CustomTheme.darkTheme);
+
+      log.debug('[theme_bloc.dart][SwitchedTheme]: isDarkTheme: $isDarkTheme');
+
+      _onSetTheme(isDarkTheme);
+    });
   }
 
   Future<bool> _isDarkMode() async {
-    // TODO:
-    return false;
+    final isDarkTheme = await getThemePreference();
+    return (isDarkTheme == "dark") ? true : false;
   }
 
   Future<void> _onSetTheme(bool isDarkMode) async {
-    // TODO:
-    if (isDarkMode) {
+    final prefs = locator.get<LocalStorageService>();
+    await prefs.saveDataToStorage(
+        key: LocalStorageKey.themeMode,
+        value: (isDarkMode == true) ? "dark" : "light");
+  }
 
-    } else {
-
-    }
+  Future<String?> getThemePreference() async {
+    final prefs = locator.get<LocalStorageService>();
+    return await prefs.readDataFromStorage(key: LocalStorageKey.themeMode);
   }
 }
