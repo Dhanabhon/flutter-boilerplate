ec40bcefd0cef8a3767bfb1996f8b556b8c924c1
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-18 22:55:55 +0700

    Update main.dart

--- flutter_boilerplate/lib/main.dart
+++ flutter_boilerplate/lib/main.dart
@@ -54,7 +54,7 @@ class MyApp extends StatelessWidget {
                 routerConfig: AppRouter.router,
                 theme: themeState,
                 darkTheme: CustomTheme.darkTheme,
-                themeMode: ThemeMode.system,
+                themeMode: CustomTheme.themeMode,
               );
             },
           );
