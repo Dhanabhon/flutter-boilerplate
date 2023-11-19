b2716a0a70345e030b5b43aa5b08dcfc50f5f177
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-19 22:48:37 +0700

    Update theme.dart

--- flutter_boilerplate/lib/config/theme.dart
+++ flutter_boilerplate/lib/config/theme.dart
@@ -31,26 +31,6 @@ class CustomTheme {
     brightness: Brightness.dark,
   );
 
-  // static final lightTheme = ThemeData(
-  //   useMaterial3: useMaterial3,
-  //   primaryColorLight: const Color(0xff486284),
-  //   scaffoldBackgroundColor: const Color(0xffF5F5F6),
-  //   buttonTheme: ButtonThemeData(
-  //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-  //     buttonColor: const Color(0xff486284),
-  //   ),
-  // );
-
-  // static final darkTheme = ThemeData(
-  //   useMaterial3: useMaterial3,
-  //   primaryColorDark: const Color(0xff151f3c),
-  //   scaffoldBackgroundColor: const Color(0xffF5F5F6),
-  //   buttonTheme: ButtonThemeData(
-  //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-  //     buttonColor: const Color(0xff486284),
-  //   ),
-  // );
-
   static bool isDarkMode(BuildContext context) =>
       Theme.of(context).brightness == Brightness.dark;
 }
