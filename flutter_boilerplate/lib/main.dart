01fd47abc81e6b58fe5d7c7cf7b64b8b08571216
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-19 22:41:03 +0700

    Update main.dart

--- flutter_boilerplate/lib/main.dart
+++ flutter_boilerplate/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -17,6 +18,7 @@ import 'package:flutter_boilerplate/blocs/language/language_event.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   initializeApp();
   runApp(const MyApp());
 }
