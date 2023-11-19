39cb8ef03f4615ce0555a1063e508beeb0dbde28
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-19 22:40:51 +0700

    Update custom_elevated_button.dart

--- flutter_boilerplate/lib/widgets/custom_elevated_button.dart
+++ flutter_boilerplate/lib/widgets/custom_elevated_button.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class CustomElevatedButton extends StatelessWidget {
   const CustomElevatedButton(
       {super.key,
       required this.text,
-      this.isEnabledShadowColor = true,
+      this.isFullWidthButton = true,
       required this.onPressed});
 
   final String text;
-  final bool isEnabledShadowColor;
+  final bool isFullWidthButton;
   final VoidCallback onPressed;
 
   @override
@@ -17,30 +19,12 @@ class CustomElevatedButton extends StatelessWidget {
       builder: (BuildContext context, BoxConstraints constraints) {
         return ElevatedButton(
           onPressed: () => onPressed(),
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all<EdgeInsets>(
-                const EdgeInsets.symmetric(horizontal: 16.0)),
-            backgroundColor:
-                MaterialStateProperty.all<Color>(const Color(0xff486284)),
-            shadowColor: isEnabledShadowColor
-                ? MaterialStateProperty.all<Color>(Colors.grey)
-                : null,
-            elevation: MaterialStateProperty.resolveWith<double>(
-                (Set<MaterialState> states) {
-              if (states.contains(MaterialState.pressed)) return 10;
-              return 5;
-            }),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-            ),
-            animationDuration: const Duration(milliseconds: 200),
-            minimumSize: MaterialStateProperty.all<Size>(
-                Size(constraints.maxWidth, 64.0)),
-            textStyle: MaterialStateProperty.all<TextStyle>(
-                const TextStyle(fontSize: 16.0)),
-          ),
+          style: (isFullWidthButton)
+              ? ButtonStyle(
+                  minimumSize: MaterialStateProperty.all<Size>(
+                      Size(constraints.maxWidth, 40.h)),
+                )
+              : null,
           child: Text(text),
         );
       },
