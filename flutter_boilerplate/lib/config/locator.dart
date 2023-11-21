47dfbdc74b3721151993eb55f62bb700d309ba3e
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-11-22 01:58:20 +0700

    Update locator.dart

--- flutter_boilerplate/lib/config/locator.dart
+++ flutter_boilerplate/lib/config/locator.dart
@@ -4,7 +4,7 @@ import 'package:watch_it/watch_it.dart';
 // import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:flutter_boilerplate/config/logger.dart';
-import 'package:flutter_boilerplate/routes/routes.dart';
+import 'package:flutter_boilerplate/routes/router.dart';
 import 'package:flutter_boilerplate/services/local_storage.dart';
 import 'package:flutter_boilerplate/blocs/theme/theme_bloc.dart';
 import 'package:flutter_boilerplate/blocs/language/language_bloc.dart';
